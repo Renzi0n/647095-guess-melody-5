8a1ce3edb0a8d167795b35a42e52869e43e7ca65
Andrew <andersen7298@gmail.com>
2020-10-04 19:35:15 +0300

    Новые компоненты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import PropTypes from "prop-types";
-import WelcomeScreen from "../welcome-screen/welcome-screen";
+import React from 'react';
+import PropTypes from 'prop-types';
+import WelcomeScreen from '../welcome-screen/welcome-screen';
 
 
 const App = (props) => {
